c8b4517a743e7c9cac78e51de30db4c522c83021
agata-m <49907570+agata-m@users.noreply.github.com>
2019-10-01 18:58:43 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = (env) => {
+	const environment = env || 'production';
+
+	return {
+		mode: environment,
+		entry: './src/app.js',
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'app.' + environment + 'bundle.js'
+		}
+	}
+};
